c9a062baa5c1bab3a44b6acea1a8d4a8a24e5c22
Anwesh Budhathoki <abperni@gmail.com>
2021-10-18 20:23:41 +0545

    Add beforedeploy travis

--- frontend/src/components/constants/navbar/NavBar.tsx
+++ frontend/src/components/constants/navbar/NavBar.tsx
@@ -2,10 +2,12 @@ import history from "utils/history";
 import * as routes from 'constants/routes';
 
 const NavBar = () => {
-    return (<div>
-        <span onClick={()=>history.push(routes.BUILDER)}>Builder</span>
-        <span onClick={()=>history.push(routes.HOME)}>Forms</span>
-    </div>);
+    return (
+        <div>
+            <span onClick={() => history.push(routes.BUILDER)}>Builder</span>
+            <span onClick={() => history.push(routes.HOME)}>Forms</span>
+        </div>
+    );
 }
 
 export default NavBar;
