190fd9a836368c479c0d27f6d396297e4be74065
Anwesh Budhathoki <abperni@gmail.com>
2021-10-18 19:55:04 +0545

    Travis deploy test

--- frontend/src/components/constants/navbar/NavBar.tsx
+++ frontend/src/components/constants/navbar/NavBar.tsx
@@ -2,10 +2,10 @@ import history from "utils/history";
 import * as routes from 'constants/routes';
 
 const NavBar = () => {
-    return (<>
+    return (<div>
         <span onClick={()=>history.push(routes.BUILDER)}>Builder</span>
         <span onClick={()=>history.push(routes.HOME)}>Forms</span>
-    </>);
+    </div>);
 }
 
 export default NavBar;
